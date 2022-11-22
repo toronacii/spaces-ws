c4509f15d2ec9bb962088fab5f5ad8bfd03f09a1
Jose Toro <toronacii@AB-140.local>
2022-11-22 06:29:17 -0300

    first commit

---
+++ yjs-server.ts
@@ -0,0 +1,30 @@
+import { Logger } from '@hocuspocus/extension-logger';
+import { Server } from '@hocuspocus/server';
+import { slateNodesToInsertDelta } from '@slate-yjs/core';
+import * as Y from 'yjs';
+
+const initialValue = [{ type: 'paragraph', children: [{ text: 'Demo' }] }];
+
+// Setup the server
+const server = Server.configure({
+  port: 4000,
+
+  // Add logging
+  extensions: [new Logger()],
+
+  async onLoadDocument(data) {
+    // Load the initial value in case the document is empty
+    if (data.document.isEmpty('content')) {
+      const insertDelta = slateNodesToInsertDelta(initialValue);
+      const sharedRoot = data.document.get('content', Y.XmlText);
+      // @ts-ignore
+      sharedRoot.applyDelta(insertDelta);
+    }
+
+    return data.document;
+  },
+});
+
+// Start the server
+server.enableMessageLogging();
+server.listen();
